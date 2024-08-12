96f1b06954ab8cbf62b13657c06afdbcfe9914bc
YjieKim <yjiekim@gmail.com>
2024-08-12 15:26:07 +0900

    Add files via upload

--- mProject_mbti.py
+++ mProject_mbti.py
@@ -18,7 +18,7 @@ def mbti_main():
     mbti_list = ["ISTJ", "ISFJ", "INFJ", "INTJ", "ISTP", "ISFP", "INFP", "INTP",
                 "ESTP", "ESFP", "ENFP", "ENTP", "ESTJ", "ESFJ", "ENFJ", "ENTJ"]
     
-    user_mbti = ''
+    # user_mbti = ''
     user_mbti = st.selectbox(
         "당신의 MBTI는 무엇인가요?",
         mbti_list,
@@ -26,33 +26,34 @@ def mbti_main():
         placeholder="MBTI를 선택하세요.",
     )
 
-    st.write(f"당신의 MBTI는 {user_mbti}이네요.")
+    with st.expander('') :
 
-    if user_mbti != '':
-        recommend_food = mbti_fd.re_food(user_mbti)   # 음식 추천
-        recommend_workout = mbti_wo.get_random_workout(user_mbti)   # 운동 추천
-        recommend_movie, reason = mbti_mv.recommend_movie(user_mbti)    # 영화 추천
+        if user_mbti is not None:
 
-        st.write(f"""
-    
-        당신의 MBTI는 {user_mbti}네요.
-        그럼 {user_mbti}인 당신만을 위한 추천 드립니다.
+            st.write(f"당신의 MBTI는 {user_mbti}이네요.")
 
-        이번 주말에는 {recommend_food}를 드셔보세요.
+            recommend_food = mbti_fd.re_food(user_mbti)   # 음식 추천
+            recommend_workout = mbti_wo.get_random_workout(user_mbti)   # 운동 추천
+            recommend_movie, reason = mbti_mv.recommend_movie(user_mbti)    # 영화 추천
 
-        (아주 많이) 덥고 습하지만… 운동도 잊으면 안 되죠.
-        {recommend_workout}도 한 번 해보세요~
+            st.write(f"""
+        
+            당신의 MBTI는 {user_mbti}네요.
+            그럼 {user_mbti}인 당신만을 위한 추천 드립니다.
 
-        마지막으로 시원한 맥주와 함께
-        '{recommend_movie}' 영화를 보며 주말 마무리를 해 보아요.
-        {reason}
+            이번 주말에는 {recommend_food}를 드셔보세요.
 
-        그럼 행복한 주말 보내기를 바랍니다.
-        다음 주에도 심심하면 다시 방문해 주세요~
+            (아주 많이) 덥고 습하지만… 운동도 잊으면 안 되죠.
+            {recommend_workout}도 한 번 해보세요~
 
-        """)
-    else:
-        st.write("기다리는 중이에요.")
+            마지막으로 시원한 맥주와 함께
+            '{recommend_movie}' 영화를 보며 주말 마무리를 해 보아요.
+            {reason}
+
+            그럼 행복한 주말 보내기를 바랍니다.
+            다음 주에도 심심하면 다시 방문해 주세요~
+
+            """)
 
 
 if __name__=='__main__':
